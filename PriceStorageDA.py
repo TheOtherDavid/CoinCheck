fe538363196c41bff72d9f01cbb1c845a26604ba
David <davidhand1988@gmail.com>
2021-01-18 16:27:47 -0500

    Adding method to return all prices for a product.

--- PriceStorageDA.py
+++ PriceStorageDA.py
@@ -11,3 +11,14 @@ class PriceStorageDA:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerow(row)
 
+    def getPrices(self, product_code):
+        filename = "prices.csv"
+
+        rows = []
+        with open(filename, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for row in csv_reader:
+                # Add row to response if the row isn't empty.
+                if row:
+                    rows.append(row)
+        return rows
